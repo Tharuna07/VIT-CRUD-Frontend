ebc37e0850362aa2ba5bfee7e09596489c895483
Tharuna07 <tharunasrilakshmi7@gmail.com>
2025-07-08 17:01:49 +0530

    Describe your changes, e.g., Remove deprecated mongoose options and fix build warnings

--- backend/index.js
+++ backend/index.js
@@ -24,9 +24,6 @@ const dbConnect = async () => {
     const options = {
       serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
       socketTimeoutMS: 45000, // Close sockets after 45s of inactivity
-      bufferMaxEntries: 0, // Disable mongoose buffering
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     };
 
     await mongoose.connect(process.env.MONGO_URI, options);
