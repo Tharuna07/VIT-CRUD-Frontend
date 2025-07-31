3664f27213ee30fb7d4113c23fabc6969dc47cb8
Tharuna07 <tharunasrilakshmi7@gmail.com>
2025-07-31 11:58:41 +0530

    modified

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,23 +1,32 @@
 import React from "react";
 import "../Styles/Footer.css";
 import keep from "./keep.png";
+
 function Footer() {
   return (
     <div className="con">
-      <h3 className="d1">Experience shoping on modile</h3>
+      <h3 className="d1">Experience shopping on mobile</h3>
       <h4 className="c5">Keep in Touch</h4>
+
+      {/* Fixed anchor tag and added alt */}
       <div>
         <li>
-          <a href="">
-            <img src={keep} className="s9"></img>
+          <a
+            href="https://example.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={keep} className="s9" alt="Stay connected" />
           </a>
         </li>
       </div>
+
       <div className="v1">
-        <h7 className="vi">popular searches</h7>
+        <h6 className="vi">Popular Searches</h6>
         <p>
-          shirts|skrits|teens|Trousers|jeeans|Biba|kurtas|faceoils|facecream|facewash|serum|shampoo|{" "}
-          <a href="">contact us</a>
+          shirts | skirts | teens | trousers | jeans | Biba | kurtas | face oils
+          | face cream | face wash | serum | shampoo | {/* Fixed anchor tag */}
+          <a href="/contact">Contact Us</a>
         </p>
       </div>
     </div>
